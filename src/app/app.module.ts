7cb2ffe0cff62bab673b5f2c64717f64740a8fea
Jakob Aarøe Dam <CONS_JAA@jysk.com>
2022-06-19 23:05:28 +0200

    add directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PositionDirective } from './position.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PositionDirective
   ],
   imports: [
     BrowserModule
